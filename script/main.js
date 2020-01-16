d39bc4bf9aafc14e3b1b74e1a30a7ccf026f6074
Zach Forrest <ZachForrest@Zachs-MBP.attlocal.net>
2020-01-15 23:43:10 -0500

    made the time display live and added animation

--- script/main.js
+++ script/main.js
@@ -1,73 +1,87 @@
 (function() {
   'use strict;'
 
+  function clock() {
 
-  let getCurrentTime = () => {
-    currentTime = new Date();
-    return currentTime;
-    // dont forget the return
+    const currentDate = new Date();
+    // pulling the time and adding 0's before and :'s after
+    let time = zero(currentDate.getHours()) + ":" + zero(currentDate.getMinutes()) + ":" + zero(currentDate.getSeconds());
+    // replacing the spans content with the set variable
+    document.getElementById("digital-clock").innerHTML = time;
+
+    setInterval(clock, 1000);
+  }
+  // allowing the zero to be added
+  function zero(i) {
+    if (i < 10) {
+      return "0" + i;
+    } else {
+      return i;
+    }
   }
 
-  let $clock = document.getElementById('clock');
-  console.log('here is the console log', $clock);
-  
-
-  $clock.innerHTML = getCurrentTime();
+  clock();
+  zeros();
 
 
+  // clock that did not work using 3 spans with ids (hours counted up with seconds until 10 seconds)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  //  function clock() {
+  //    const fullDate = new Date();
+  //    var hours = fullDate.getHours();
+  //    var minutes = fullDate.getMinutes();
+  //    var seconds = fullDate.getSeconds();
+  //    if (hours < 10) {
+  //      hours = "0" + hours;
+  //    }
+  //    if (minutes < 10) {
+  //      hours = "0" + minutes;
+  //    }
+  //    if (seconds < 10) {
+  //      hours = "0" + seconds;
+  //    }
+  //    document.getElementById('hour').innerHTML = hours;
+  //    document.getElementById('minute').innerHTML = ":" + minutes;
+  //    document.getElementById('second').innerHTML = ":" + seconds;
+  //
+  //  }
+  //
+  // setInterval(clock,1000);
 
 
 
 
 
-// let $element = document.querySelector('span');
-// console.log($element);
-//
-// $element.textContent = "Hello, Sally!"
-//
-//
-// let $saying = document.querySelector('#saying')
-// console.log($saying.textContent);
-//
-// $saying.textContent = "That's all folks."
 
-// let date = new Date();
-// console.log(date);
 
-// let getCurrentTime = () => {
-//   currentTime = new Date();
-//   console.log(currentTime)
-// }
-// //setInterval(function, ms)
-// setInterval(getCurrentTime,1000);
-//
 
-// let $button = document.querySelector('button');
-// $button.addEventListener('click', () => {
-//   console.log('You clicked the button')
-// });
+
+  // let $element = document.querySelector('span');
+  // console.log($element);
+  //
+  // $element.textContent = "Hello, Sally!"
+  //
+  //
+  // let $saying = document.querySelector('#saying')
+  // console.log($saying.textContent);
+  //
+  // $saying.textContent = "That's all folks."
+
+  // let date = new Date();
+  // console.log(date);
+
+  // let getCurrentTime = () => {
+  //   currentTime = new Date();
+  //   console.log(currentTime)
+  // }
+  // //setInterval(function, ms)
+  // setInterval(getCurrentTime,1000);
+  //
+
+  // let $button = document.querySelector('button');
+  // $button.addEventListener('click', () => {
+  //   console.log('You clicked the button')
+  // });
 
 
 
